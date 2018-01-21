17610de7bdbcbb31930d3b45bb22bb0efbb95bd1
papergo <paper_go@126.com>
2018-01-21 11:42:06 +0800

    修改JSON路径，源文件路径及头文件引用同步

--- MDK-ARM/YS-F1Mini/bsp_gsm.d
+++ MDK-ARM/YS-F1Mini/bsp_gsm.d
@@ -1,7 +1,7 @@
 .\ys-f1mini\bsp_gsm.o: ..\Src\bsp\gsm\bsp_gsm.c
-.\ys-f1mini\bsp_gsm.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdarg.h
-.\ys-f1mini\bsp_gsm.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-.\ys-f1mini\bsp_gsm.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\ys-f1mini\bsp_gsm.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdarg.h
+.\ys-f1mini\bsp_gsm.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\ys-f1mini\bsp_gsm.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
 .\ys-f1mini\bsp_gsm.o: ..\Inc\bsp\usart/bsp_debug_usart.h
 .\ys-f1mini\bsp_gsm.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
 .\ys-f1mini\bsp_gsm.o: ../Inc/stm32f1xx_hal_conf.h
@@ -11,14 +11,14 @@
 .\ys-f1mini\bsp_gsm.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
 .\ys-f1mini\bsp_gsm.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h
 .\ys-f1mini\bsp_gsm.o: ../Drivers/CMSIS/Include/core_cm3.h
-.\ys-f1mini\bsp_gsm.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\ys-f1mini\bsp_gsm.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\ys-f1mini\bsp_gsm.o: ../Drivers/CMSIS/Include/core_cmInstr.h
 .\ys-f1mini\bsp_gsm.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
 .\ys-f1mini\bsp_gsm.o: ../Drivers/CMSIS/Include/core_cmFunc.h
 .\ys-f1mini\bsp_gsm.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
 .\ys-f1mini\bsp_gsm.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
 .\ys-f1mini\bsp_gsm.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
-.\ys-f1mini\bsp_gsm.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\ys-f1mini\bsp_gsm.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
 .\ys-f1mini\bsp_gsm.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h
 .\ys-f1mini\bsp_gsm.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h
 .\ys-f1mini\bsp_gsm.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h
--- MDK-ARM/YS-F1Mini/stm32f1xx_hal_rtc.d
+++ MDK-ARM/YS-F1Mini/stm32f1xx_hal_rtc.d
@@ -7,14 +7,14 @@
 .\ys-f1mini\stm32f1xx_hal_rtc.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
 .\ys-f1mini\stm32f1xx_hal_rtc.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h
 .\ys-f1mini\stm32f1xx_hal_rtc.o: ../Drivers/CMSIS/Include/core_cm3.h
-.\ys-f1mini\stm32f1xx_hal_rtc.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\ys-f1mini\stm32f1xx_hal_rtc.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\ys-f1mini\stm32f1xx_hal_rtc.o: ../Drivers/CMSIS/Include/core_cmInstr.h
 .\ys-f1mini\stm32f1xx_hal_rtc.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
 .\ys-f1mini\stm32f1xx_hal_rtc.o: ../Drivers/CMSIS/Include/core_cmFunc.h
 .\ys-f1mini\stm32f1xx_hal_rtc.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
 .\ys-f1mini\stm32f1xx_hal_rtc.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
 .\ys-f1mini\stm32f1xx_hal_rtc.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
-.\ys-f1mini\stm32f1xx_hal_rtc.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\ys-f1mini\stm32f1xx_hal_rtc.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
 .\ys-f1mini\stm32f1xx_hal_rtc.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h
 .\ys-f1mini\stm32f1xx_hal_rtc.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h
 .\ys-f1mini\stm32f1xx_hal_rtc.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h
